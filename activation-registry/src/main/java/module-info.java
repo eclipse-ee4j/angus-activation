b9416666a9002f8f31ce6858b794b54a5b958e13
Lukas Jungmann <lukas.jungmann@oracle.com>
2022-12-09 12:23:43 +0100

    Support native image
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- activation-registry/src/main/java/module-info.java
+++ activation-registry/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -15,6 +15,8 @@ module com.sun.activation.registries {
     exports com.sun.activation.registries;
     requires java.logging;
     requires transitive jakarta.activation;
+
+    requires static org.graalvm.sdk;
 
     provides jakarta.activation.spi.MailcapRegistryProvider with com.sun.activation.registries.MailcapRegistryProviderImpl;
     provides jakarta.activation.spi.MimeTypeRegistryProvider with com.sun.activation.registries.MimeTypeRegistryProviderImpl;
